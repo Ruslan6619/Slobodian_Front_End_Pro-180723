6fce8210ca6669e4c684a597299e2e255dd2d869
Ruslan6619 <ruslanslobodyan1994@gmail.com>
2023-07-24 17:01:33 +0300

    number of seconds

--- js/hw2/scripts.js
+++ js/hw2/scripts.js
@@ -1,74 +1,74 @@
 //1
-let years = prompt('Enter User name')
-alert(`Hello, ${years}! How are you?`);
-
-
-
-//2
-let defaultValue = 0;
-let firstNumber = parseInt(prompt('Enter firs number', defaultValue));
-let secondNumber = parseInt(prompt('Enter second number', defaultValue));
-
-let sum = firstNumber + secondNumber;
-console.log(sum)
-
-let minus = firstNumber - secondNumber;
-console.log(minus)
-
-let multiply = firstNumber * secondNumber;
-console.log(multiply)
-
-let divide = firstNumber / secondNumber;
-console.log(divide)
-
-
-
-//3
-
-let firstNumber2 = prompt('Enter firs number');
-let secondNumber2 = prompt('Enter second number');
-
-let strictEquality = firstNumber2 === secondNumber2;
-console.log(strictEquality);
-
-
-//4
-
-let firstNumber3 = parseInt(prompt('Enter firs number'));
-let secondNumber3 = parseInt(prompt('Enter second number'));
-let thirdNumber = parseInt(prompt('Enter third number'));
-let totalNumber = 3 ;
-
-let averageValues = (firstNumber3 + secondNumber3 + thirdNumber) / totalNumber ;
-console.log(averageValues);
-
-
-
-
-// //5
-
-let digit = prompt('Введите 5 цифр');
-let modularDivision1 = digit % 10;
-
-
-let minus1 = digit - 0;
-let divide1 = parseInt(minus1 / 10) ;
-let modularDivision2 = divide1 % 10;
-
-
-let minus2 = digit - 0 ;
-let divide2 = parseInt(minus2 / 100);
-let modularDivision3 = divide2 % 10 ;
-
-
-
-let minus3 = digit - 0 ;
-let divide3 = parseInt(minus3 / 1000);
-let modularDivision4 = divide3 % 10 ;
-
-
-let minus4 = digit - 0 ;
-let divide4 = parseInt(minus4 / 10000);
-let modularDivision5 = divide4 % 10 ;
-
-console.log( modularDivision5, modularDivision4, modularDivision3, modularDivision2, modularDivision1);
+// let years = prompt('Enter User name')
+// alert(`Hello, ${years}! How are you?`);
+//
+//
+//
+// //2
+// let defaultValue = 0;
+// let firstNumber = parseInt(prompt('Enter firs number', defaultValue));
+// let secondNumber = parseInt(prompt('Enter second number', defaultValue));
+//
+// let sum = firstNumber + secondNumber;
+// console.log(sum)
+//
+// let minus = firstNumber - secondNumber;
+// console.log(minus)
+//
+// let multiply = firstNumber * secondNumber;
+// console.log(multiply)
+//
+// let divide = firstNumber / secondNumber;
+// console.log(divide)
+//
+//
+//
+// //3
+//
+// let firstNumber2 = prompt('Enter firs number');
+// let secondNumber2 = prompt('Enter second number');
+//
+// let strictEquality = firstNumber2 === secondNumber2;
+// console.log(strictEquality);
+//
+//
+// //4
+//
+// let firstNumber3 = parseInt(prompt('Enter firs number'));
+// let secondNumber3 = parseInt(prompt('Enter second number'));
+// let thirdNumber = parseInt(prompt('Enter third number'));
+// let totalNumber = 3 ;
+//
+// let averageValues = (firstNumber3 + secondNumber3 + thirdNumber) / totalNumber ;
+// console.log(averageValues);
+//
+//
+//
+//
+// // //5
+//
+// let digit = prompt('Введите 5 цифр');
+// let modularDivision1 = digit % 10;
+//
+//
+// let minus1 = digit - 0;
+// let divide1 = parseInt(minus1 / 10) ;
+// let modularDivision2 = divide1 % 10;
+//
+//
+// let minus2 = digit - 0 ;
+// let divide2 = parseInt(minus2 / 100);
+// let modularDivision3 = divide2 % 10 ;
+//
+//
+//
+// let minus3 = digit - 0 ;
+// let divide3 = parseInt(minus3 / 1000);
+// let modularDivision4 = divide3 % 10 ;
+//
+//
+// let minus4 = digit - 0 ;
+// let divide4 = parseInt(minus4 / 10000);
+// let modularDivision5 = divide4 % 10 ;
+//
+// console.log( modularDivision5, modularDivision4, modularDivision3, modularDivision2, modularDivision1);
---
+++ js/hw3/scripts.js
@@ -0,0 +1,4 @@
+
+let time = prompt('How much time?');
+let timeSeconds = time  * 60 * 60;
+alert(`at ${time} o'clock ${timeSeconds} seconds` );
