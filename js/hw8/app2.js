763b82ab4d280b57392fc32f47831fc4fb31b389
Ruslan6619 <ruslanslobodyan1994@gmail.com>
2023-08-07 16:29:34 +0300

    homework 8

--- js/hw8/app2.js
+++ js/hw8/app2.js
@@ -1,10 +1,10 @@
 let arr = [16,-37,54,-4,72,-56,47,4,-16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47] ;
 let sum = 0 ;
 let quantity = 0;
-let minValue = Math.min.apply(null, arr);
-let positionMin = arr.indexOf(minValue) ;
-let maxValue = Math.max.apply(null, arr);
-let positionMax = arr.indexOf(maxValue) ;
+let maxValue = arr[0];
+let positionMax =  0;
+let minValue = arr[0];
+let positionMin = 0;
 let numberOfNegative = 0;
 let evenPositiveElements = 0;
 let oddPositiveElements = 0;
@@ -19,15 +19,27 @@ for (let i = 0 ; i <= arr.length ; i++){
     if (arr[i] > 0){
         sum += arr[i]
         quantity += arr[i] > 0
-    } if (arr[i] < 0){
+    }
+    if (arr[i] < minValue){
+        minValue = arr[i]
+        positionMin = i
+    }
+    if (arr[i] > maxValue){
+        maxValue = arr[i]
+        positionMax = i
+    }
+    if (arr[i] < 0){
         numberOfNegative += arr[i] < 0
-    } if (arr[i] > 0 && arr[i] % 2 === 0){
+    }
+    if (arr[i] > 0 && arr[i] % 2 === 0){
         sumEvenPositiveElements += arr[i]
         evenPositiveElements++
-    }if (arr[i] > 0 && arr[i] % 2 !== 0){
+    }
+    if (arr[i] > 0 && arr[i] % 2 !== 0){
         sumOfOddPositiveElements += arr[i]
         oddPositiveElements ++
-    }if (arr[i] > 0){
+    }
+    if (arr[i] > 0){
         productOfPositiveElements *= arr[i]
     }
 
