ba060068a96432fff8751baf1a77c7a5b2b74b79
Ruslan6619 <ruslanslobodyan1994@gmail.com>
2023-09-07 02:28:23 +0300

    homework 20

--- js/hw20/app.js
+++ js/hw20/app.js
@@ -1,21 +1,40 @@
-function table(){
-const form = document.forms.mainForm;
-const userDiv = document.getElementById('userData');
-const userList = document.getElementById("userList");
-const formData = new FormData(form);
+function showTable() {
+    const form = document.forms.mainForm;
+    const userDiv = document.getElementById('userData');
+    const userList = document.getElementById("userList");
+    const formData = new FormData(form);
+    let selectedLanguages = [];
 
-for (let [fieldName, fieldValue] of formData){
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `${fieldName}:  ${fieldValue}`;
-    userList.appendChild(listItem);
-}
-userDiv.style.display = 'block';
-form.style.display = 'none';
+    for (let [fieldName, fieldValue] of formData) {
+        if (fieldName !== 'languages') {
+            const listItem = document.createElement('li');
+            listItem.innerHTML = `${fieldName}:  ${fieldValue}`;
+            userList.appendChild(listItem);
+        }
+    }
+
+    const languageCheckboxes = document.querySelectorAll('[name = "languages"]');
+
+    for (let i = 0; i < languageCheckboxes.length; i++) {
+        if (languageCheckboxes[i].checked) {
+            selectedLanguages.push(languageCheckboxes[i].value)
+        }
+    }
+
+    const languagesItem = document.createElement('li');
+
+    if (selectedLanguages.length === 1) {
+        languagesItem.innerHTML = `Languages : ${selectedLanguages[0]}`
+    } else if (selectedLanguages.length > 1) {
+        languagesItem.innerHTML = `Languages : ${selectedLanguages.join(', ')}`
+    }
+
+    userList.appendChild(languagesItem);
+
+    userDiv.style.display = 'block';
+    form.style.display = 'none';
 }
 
-document.getElementById('save').addEventListener('click', function (){
-    table();
+document.getElementById('save').addEventListener('click', function () {
+    showTable();
 });
-
-
-
