1f09c9cdd3674c40b39fab6f55171a15815476f0
Ruslan6619 <ruslanslobodyan1994@gmail.com>
2023-09-07 22:59:14 +0300

    homework 21

--- js/hw21/app.js
+++ js/hw21/app.js
@@ -34,6 +34,9 @@ function productDescription(product, category) {
     const parentElement = document.getElementById('center');
     const parentElementRight = document.getElementById('right');
     const main = document.getElementById('main');
+    const wrapper = document.getElementById('wrapper');
+    const boxOrderForm = document.getElementById('boxOrderForm');
+
     parentElementRight.innerHTML = '';
 
 
@@ -48,13 +51,13 @@ function productDescription(product, category) {
     btn.textContent = `${'Купить'}`;
     btn.style.backgroundColor = 'aqua'
     btn.addEventListener('click', function () {
-        let purchaseMessage = document.createElement('div');
-        purchaseMessage.classList.add('purchasedGoods')
-        purchaseMessage.textContent = `Товар ${product.name} куплен за $${product.price}`;
+        let purchaseMessage = document.getElementById('boxOrderForm');
+        const orderDetails = document.getElementById('orderDetails');
+        orderDetails.textContent = `Товар ${product.name} куплен за $${product.price}`
         parentElement.innerHTML = '';
         parentElementRight.innerHTML = '';
-
-        main.appendChild(purchaseMessage);
+        wrapper.appendChild(purchaseMessage);
+        boxOrderForm.style.display = 'block'
 
     })
     parentElementRight.appendChild(btn);
@@ -64,6 +67,15 @@ function productDescription(product, category) {
 
 showCategories();
 
+function hideElements(boxOrderForm, orderSummary) {
+    const elementOrderForm = document.getElementById(boxOrderForm);
+    const elementOrderSummary = document.getElementById(orderSummary);
+    if (elementOrderForm && elementOrderSummary) {
+        elementOrderForm.style.display = 'none';
+        elementOrderSummary.style.display = 'none';
+    }
+}
+
 
 document.getElementById('left').addEventListener('click', event => {
     if (event.target.nodeName === 'DIV') {
@@ -71,10 +83,11 @@ document.getElementById('left').addEventListener('click', event => {
         const categoryProducts = categories[categoryKey].products;
         const purchaseMessage = document.querySelector('.purchasedGoods');
 
+        hideElements('boxOrderForm', 'orderSummary');
+
         if (purchaseMessage) {
             purchaseMessage.remove();
         }
-
         showProducts(categoryProducts, categoryKey);
     }
 });
@@ -89,5 +102,60 @@ document.getElementById('center').addEventListener('click', event => {
         productDescription(product, categoryKey);
     }
 });
+
+
+document.getElementById('orderForm').addEventListener('submit', function (event) {
+    event.preventDefault(); // Предотвращаем отправку формы, чтобы не обновлять страницу
+
+    const fullName = document.getElementById('fullName');
+    const city = document.getElementById('city');
+    const novaPoshta = document.getElementById('novaPoshta');
+    const paymentMethod = document.querySelector('input[name="Оплата"]:checked');
+    const quantity = document.getElementById('quantity');
+    const form = document.getElementById('orderForm');
+    const boxForm = document.getElementById('boxOrderForm');
+    const orderSummary = document.getElementById('orderSummary');
+    const orderDetails = document.getElementById('orderDetails');
+    let hasErrors = false;
+
+    if (fullName.value.trim() === '') {
+        hasErrors = true;
+    }
+
+    if (city.value === '') {
+        hasErrors = true;
+    }
+
+    if (novaPoshta.value.trim() === '') {
+        hasErrors = true;
+    }
+
+    if (!paymentMethod) {
+        hasErrors = true;
+    }
+
+    if (quantity.value.trim() === '') {
+        hasErrors = true;
+    }
+
+    if (hasErrors) {
+        return;
+    }
+
+    const formData = new FormData(form);
+
+    for (const [fieldName, fieldValue] of formData) {
+        const paragraph = document.createElement('p');
+        paragraph.textContent = `${fieldName}: ${fieldValue}`;
+        orderDetails.appendChild(paragraph);
+    }
+    orderSummary.style.display = 'block';
+    boxForm.style.display = 'none';
+
+    form.reset();
+
+});
+
+
 
 
