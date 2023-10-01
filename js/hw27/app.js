3726b52b4a0336b0988656809c2e64e3f4e2a7d9
Ruslan6619 <ruslanslobodyan1994@gmail.com>
2023-10-01 22:16:29 +0300

    hw27

--- js/hw27/app.js
+++ js/hw27/app.js
@@ -1,1 +1,42 @@
-document.querySelector("div").textContent = moment().calendar()
+
+    const slides = document.querySelectorAll('.slide');
+    const prevButton = document.getElementById('prevButton');
+    const nextButton = document.getElementById('nextButton');
+    let currentIndex = 0;
+    let intervalId;
+
+    function showSlide(index) {
+        for (let i = 0; i < slides.length; i++) {
+            const slide = slides[i];
+            if (i === index) {
+                slide.classList.add('active');
+            } else {
+                slide.classList.remove('active');
+            }
+        }
+    }
+
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % slides.length;
+        showSlide(currentIndex);
+        clearInterval(intervalId);
+        startInterval();
+    }
+
+    function prevSlide() {
+        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+        showSlide(currentIndex);
+        clearInterval(intervalId);
+        startInterval();
+    }
+
+    function startInterval() {
+        intervalId = setInterval(nextSlide, 3000);
+    }
+
+    showSlide(currentIndex);
+    nextButton.addEventListener('click', nextSlide);
+    prevButton.addEventListener('click', prevSlide);
+
+    startInterval();
+
