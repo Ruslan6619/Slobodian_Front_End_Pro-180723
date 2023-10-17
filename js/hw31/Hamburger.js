9e42e0e7f9a76907a07288dafe23f1d2c672a933
Ruslan6619 <ruslanslobodyan1994@gmail.com>
2023-10-17 17:59:53 +0300

    hw31

--- js/hw31/Hamburger.js
+++ js/hw31/Hamburger.js
@@ -6,69 +6,67 @@ class Hamburger {
     }
 
     static SIZE_SMALL = 'small';
+    static SIZE_LARGE = 'large';
     static STUFFING_CHEESE = 'cheese';
     static STUFFING_SALAD = 'salad';
     static STUFFING_POTATO = 'potato';
     static TOPPING_MAYO = 'mayo';
     static TOPPING_SAUCE = 'sauce';
 
+    static TOPPING_PRICES = {
+        [Hamburger.TOPPING_MAYO]: 20,
+        [Hamburger.TOPPING_SAUCE]: 15,
+    };
+
+    static STUFFING_PRICES = {
+        [Hamburger.STUFFING_CHEESE]: 10,
+        [Hamburger.STUFFING_SALAD]: 20,
+        [Hamburger.STUFFING_POTATO]: 15,
+    };
+
+    static SIZE_PRICES = {
+        [Hamburger.SIZE_SMALL]: 50,
+        [Hamburger.SIZE_LARGE]: 100,
+    };
+
+    static TOPPING_CALORIES = {
+        [Hamburger.TOPPING_MAYO]: 5,
+        [Hamburger.TOPPING_SAUCE]: 5,
+    };
+
+    static STUFFING_CALORIES = {
+        [Hamburger.STUFFING_CHEESE]: 20,
+        [Hamburger.STUFFING_SALAD]: 5,
+        [Hamburger.STUFFING_POTATO]: 10,
+    };
+
     addTopping(topping) {
         this.toppings.push(topping);
     }
 
     calculatePrice() {
-        let price = 0;
-
-        if (this.size === Hamburger.SIZE_SMALL) {
-            price += 50;
-        } else {
-            price += 100;
-        }
+        let price = Hamburger.SIZE_PRICES[this.size] || 0;
 
         this.stuffing.forEach(option => {
-            if (option === Hamburger.STUFFING_CHEESE) {
-                price += 10;
-            } else if (option === Hamburger.STUFFING_SALAD) {
-                price += 20;
-            } else if (option === Hamburger.STUFFING_POTATO) {
-                price += 15;
-            }
+            price += Hamburger.STUFFING_PRICES[option] || 0;
         });
 
         this.toppings.forEach(topping => {
-            if (topping === Hamburger.TOPPING_MAYO) {
-                price += 20;
-            } else if (topping === Hamburger.TOPPING_SAUCE) {
-                price += 15;
-            }
+            price += Hamburger.TOPPING_PRICES[topping] || 0;
         });
 
         return price;
     }
 
     calculateCalories() {
-        let calories = 0;
-
-        if (this.size === Hamburger.SIZE_SMALL) {
-            calories += 20;
-        } else {
-            calories += 40;
-        }
+        let calories = this.size === Hamburger.SIZE_SMALL ? 20 : 40;
 
         this.stuffing.forEach(option => {
-            if (option === Hamburger.STUFFING_CHEESE) {
-                calories += 20;
-            } else if (option === Hamburger.STUFFING_SALAD) {
-                calories += 5;
-            } else if (option === Hamburger.STUFFING_POTATO) {
-                calories += 10;
-            }
+            calories += Hamburger.STUFFING_CALORIES[option] || 0;
         });
 
         this.toppings.forEach(topping => {
-            if (topping === Hamburger.TOPPING_MAYO || topping === Hamburger.TOPPING_SAUCE) {
-                calories += 5;
-            }
+            calories += Hamburger.TOPPING_CALORIES[topping] || 0;
         });
 
         return calories;
